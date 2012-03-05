07ba5e5e496e2233b3befd1af41d91899f9e2201
Andy Green <andygrn@gmail.com>
2012-03-05 00:03:41 +0000

    added README, cleaned up code a little

--- custard.js
+++ custard.js
@@ -1,60 +1,54 @@
 
 /*
 
-	Custard - A miniature javascript templating tool
+	Custard - A function-based javascript-to-anything templating tool
 	Andy Green
 	http://andygrn.co.uk
 	February 2012
 
 */
 
-function CustardTemplate(string){
+function Custard( string ){
 
 	this._tagSets = {};
 
 };
 
+Custard.prototype.render = function ( string, callback ){
 
-CustardTemplate.prototype.render = function(string, callback){
-
-	var tagNames = [],
-		tagBodies = [];
+	var tagNames = [];
+	var tagBodies = [];
 	
-	for ( set in this._tagSets ) {
-	
-		tagNames.push(set);
-		tagBodies.push(this._tagSets[set]);
-	
+	for ( set in this._tagSets ){
+		tagNames.push( set );
+		tagBodies.push( this._tagSets[set] );
 	}
 	
-	var template = Function(tagNames.join(), 'return [' + string + ']');
+	var template = Function( tagNames.join(), 'return [' + string + ']' );
 	
 	try {
-		callback(null, template.apply(this, tagBodies).join(''));
+		callback( null, template.apply( this, tagBodies ).join( '' ) );
 	}
-	catch (error) {
-		callback(error);
+	catch ( error ) {
+		callback( error );
 	}
 
 }
 
-CustardTemplate.prototype.addTagSet = function(setname, set){
+Custard.prototype.addTagSet = function( setname, set ){
 
 	this._tagSets[setname] = this._tagSets[setname] || {};
-	
-	for ( tagname in set ) {
-	
+	for ( tagname in set ){
 		this._tagSets[setname][tagname] = set[tagname];
-	
 	}
 
 }
 
-CustardTemplate.prototype.addTagToSet = function(setname, tagname, body){
+Custard.prototype.addTagToSet = function( setname, tagname, body ){
 
 	this._tagSets[setname] = this._tagSets[setname] || {};
 	this._tagSets[setname][tagname] = body;
 
 }
 
-module.exports = CustardTemplate;
+module.exports = Custard;
